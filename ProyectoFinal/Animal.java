5f0b63de98cbf61f837b6a86df8dfbb189daff54
Jorge González <58480988+EnigMaticSpac3@users.noreply.github.com>
2023-07-08 13:21:30 -0500

    caracteristicas clase base

--- ProyectoFinal/Animal.java
+++ ProyectoFinal/Animal.java
@@ -1,5 +1,25 @@
 // CLASE BASE
 
 public class Animal {
+    private int edad;
+    private double peso;
+    private String nombre;
+
+    public void asignar(int edad, double peso, String nombre) {
+        this.edad = edad;
+        this.peso = peso;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
 
 }
