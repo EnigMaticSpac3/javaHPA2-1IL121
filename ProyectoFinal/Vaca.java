d719ef2b28d45b98ab92b74c32081f90195534f5
amy <amy.him@utp.ac.pa>
2023-07-11 16:32:06 -0500

    verificar

--- ProyectoFinal/Vaca.java
+++ ProyectoFinal/Vaca.java
@@ -1,6 +1,14 @@
 // CLASE HIJO DE 'ANIMAL'
 
 public class Vaca extends Animal {
-    /*hola. como estass */
-    
+    private int cantidadVacas;
+
+    public void asignar(int edad, double peso, String nombreRaza, int cantidadVacas) {
+        super.asignar(edad, peso, nombreRaza);
+        this.cantidadVacas = cantidadVacas;
+    }
+
+    public int getCantidadVacas() {
+        return cantidadVacas;
+    }
 }
