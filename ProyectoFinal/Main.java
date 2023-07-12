4c3bc0209aa569ce56cb65e4cbc6d6067e361884
Jorge González <58480988+EnigMaticSpac3@users.noreply.github.com>
2023-07-11 22:58:00 -0500

    +listaOpciones - registroGallina

--- ProyectoFinal/Main.java
+++ ProyectoFinal/Main.java
@@ -5,6 +5,11 @@ public class Main {
         // Objetos
         Inventario  inventario  = new Inventario();
         Vaca        vaca        = new Vaca();
+
+        // Objetos de JAVA
+        JPanel panel = new JPanel(new BorderLayout(5,5));
+        JPanel labels, controls;
+        JTextField raza, pesoTxt;
         
         // imagenes
         ImageIcon inicio = new ImageIcon("./imgs/granja.jpg");
@@ -12,8 +17,13 @@ public class Main {
 
         // Variables
         int opcion;
-        String[] opcionesInicio = {"Acceder a Inventario", "Ventas", "Informes", "Salir"};
+        double peso;
+        String[] razasGallina       = {"Isa Brown", "Lohman", "Castellana Negra"};
+        String[] identificadores    = {"ISBWN", "LHMN", "CTLANGR"};
+        String[] opcionesInicio     = {"Acceder a Inventario", "Ventas", "Informes", "Salir"};
         String[] opcionesInventario = {"Registrar Gallina", "Registrar Vaca", "Registrar Prod.Huevos", "Registrar Prod.Leche"};
+        boolean bien, cancelado;
+
         // Inicio
         do {
             opcion = JOptionPane.showOptionDialog(null, null, "Granja", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, inicio, opcionesInicio, null);
@@ -23,27 +33,103 @@ public class Main {
                         opcion = JOptionPane.showOptionDialog(null, null, "Inventario de la Granja", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcionesInventario, null);
                         switch (opcion) {
                             case 0:
-                                // inventario.registrarGallina();
-                                break;
-                            case 1:
-                                JPanel p = new JPanel(new BorderLayout(5,5));
+                                labels = new JPanel(new GridLayout(0,1,2,2));
+                                labels.add(new JLabel("Raza", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Identificador", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Peso", SwingConstants.TRAILING));
+                                panel.add(labels, BorderLayout.LINE_START);
+                        
+                                controls = new JPanel(new GridLayout(0,1,2,2));
+                                JComboBox<String>  razaG = new JComboBox<>(razasGallina);
+                                controls.add(razaG);
+                                JTextField identificador = new JTextField(identificadores[razaG.getSelectedIndex()]);
+                                razaG.addActionListener(e -> {
+                                    identificador.setText(identificadores[razaG.getSelectedIndex()]);
+                                });
+                                controls.add(identificador);
+                                identificador.setEditable(false);
+                                pesoTxt = new JTextField();
+                                controls.add(pesoTxt);
+                                panel.add(controls, BorderLayout.CENTER);
+                        
+                                do {
+                                    bien = true;
+                                    cancelado = false;
+                                    opcion = JOptionPane.showConfirmDialog(null, panel, "Registrando Gallina", JOptionPane.OK_CANCEL_OPTION);
+                                    System.out.println("Raza: " + identificador.getText());
+                                    System.out.println("Peso: " + new String(pesoTxt.getText()));
+                                    
+                                    // si cancela
+                                    if (opcion == JOptionPane.CANCEL_OPTION) {
+                                        opcion = JOptionPane.showConfirmDialog(null, "¿Cancelar Registro de Vaca?", "Cancelando Registro de Vaca", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null);
 
-                                JPanel labels = new JPanel(new GridLayout(0,1,2,2));
+                                        if (opcion == JOptionPane.YES_OPTION) {
+                                            bien = true;
+                                            break;
+                                        } else {
+                                            bien = false;
+                                            cancelado = true;
+                                        }
+                                    }
+
+                                    // validar el peso
+                                    if (!cancelado) {
+                                        try {
+                                            peso = Double.parseDouble(pesoTxt.getText());
+                                        } catch (NumberFormatException e) {
+                                            JOptionPane.showMessageDialog(null, "El peso debe ser un numero", "Error", JOptionPane.ERROR_MESSAGE);
+                                            bien = false;
+                                        }
+                                    }
+                                } while (!bien);
+                                break;
+
+                            case 1:
+                                labels = new JPanel(new GridLayout(0,1,2,2));
                                 labels.add(new JLabel("Raza", SwingConstants.TRAILING));
                                 labels.add(new JLabel("Peso", SwingConstants.TRAILING));
-                                p.add(labels, BorderLayout.LINE_START);
+                                panel.add(labels, BorderLayout.LINE_START);
                         
-                                JPanel controls = new JPanel(new GridLayout(0,1,2,2));
-                                JTextField raza = new JTextField("Brahman");
+                                controls = new JPanel(new GridLayout(0,1,2,2));
+                                raza = new JTextField("Brahman");
                                 controls.add(raza);
                                 raza.setEditable(false);
-                                JTextField peso = new JTextField();
-                                controls.add(peso);
-                                p.add(controls, BorderLayout.CENTER);
+                                pesoTxt = new JTextField();
+                                controls.add(pesoTxt);
+                                panel.add(controls, BorderLayout.CENTER);
                         
-                                JOptionPane.showMessageDialog(null, p, "Log In", JOptionPane.QUESTION_MESSAGE);
-                                System.out.println("Raza: " + raza.getText());
-                                System.out.println("Peso: " + new String(peso.getText()));
+                                do {
+                                    bien = true;
+                                    cancelado = false;
+                                    opcion = JOptionPane.showConfirmDialog(null, panel, "Registrando Vaca", JOptionPane.OK_CANCEL_OPTION);
+                                    System.out.println("Raza: " + raza.getText());
+                                    System.out.println("Peso: " + new String(pesoTxt.getText()));
+                                    
+                                    // si cancela
+                                    if (opcion == JOptionPane.CANCEL_OPTION) {
+                                        opcion = JOptionPane.showConfirmDialog(null, "¿Cancelar Registro de Vaca?", "Cancelando Registro de Vaca", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null);
+
+                                        if (opcion == JOptionPane.YES_OPTION) {
+                                            bien = true;
+                                            break;
+                                        } else {
+                                            bien = false;
+                                            cancelado = true;
+                                        }
+                                    }
+
+                                    // validar el peso
+                                    if (!cancelado) {
+                                        try {
+                                            peso = Double.parseDouble(pesoTxt.getText());
+                                        } catch (NumberFormatException e) {
+                                            JOptionPane.showMessageDialog(null, "El peso debe ser un numero", "Error", JOptionPane.ERROR_MESSAGE);
+                                            bien = false;
+                                        }
+                                    }
+                                } while (!bien);
+
+
                         
                                 break;
                             case 2:
