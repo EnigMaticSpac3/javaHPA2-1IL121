16f67957760cd1144b3a693c956c2f792859d086
Jorge González <58480988+EnigMaticSpac3@users.noreply.github.com>
2023-07-15 16:46:17 -0500

    validaciones peso, edad

--- ProyectoFinal/Main.java
+++ ProyectoFinal/Main.java
@@ -9,14 +9,14 @@ public class Main {
         // Objetos de JAVA
         JPanel panelG, panelV;
         JPanel labels, controls;
-        JTextField raza, pesoTxt;
+        JTextField raza, edadTxt, pesoTxt;
         
         // imagenes
-        ImageIcon inicio = new ImageIcon("./imgs/granja.jpg");
+        ImageIcon inicio = new ImageIcon("./imgs/inicio.png");
 
 
         // Variables
-        int opcion;
+        int opcion, edad = 0;
         double peso = 0.0;
         String[] razasGallina       = {"Isa Brown", "Lohman", "Castellana Negra"};
         String[] identificadores    = {"ISBWN", "LHMN", "CTLANGR"};
@@ -37,7 +37,8 @@ public class Main {
                                 labels = new JPanel(new GridLayout(0,1,2,2));
                                 labels.add(new JLabel("Raza", SwingConstants.TRAILING));
                                 labels.add(new JLabel("Identificador", SwingConstants.TRAILING));
-                                labels.add(new JLabel("Peso", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Edad", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Peso (kg)", SwingConstants.TRAILING));
                                 panelG.add(labels, BorderLayout.LINE_START);
                         
                                 controls = new JPanel(new GridLayout(0,1,2,2));
@@ -49,16 +50,21 @@ public class Main {
                                 });
                                 controls.add(identificador);
                                 identificador.setEditable(false);
+                                edadTxt = new JTextField();
+                                controls.add(edadTxt);
                                 pesoTxt = new JTextField();
                                 controls.add(pesoTxt);
                                 panelG.add(controls, BorderLayout.CENTER);
                         
                                 do {
+                                    edadTxt.setText("");
+                                    pesoTxt.setText("");
                                     bien = true;
                                     cancelado = false;
                                     opcion = JOptionPane.showConfirmDialog(null, panelG, "Registrando Gallina", JOptionPane.OK_CANCEL_OPTION);
                                     System.out.println("Raza: " + identificador.getText());
-                                    System.out.println("Peso: " + new String(pesoTxt.getText()));
+                                    System.out.println("Edad: " + edadTxt.getText());
+                                    System.out.println("Peso (kg): " + new String(pesoTxt.getText()));
                                     
                                     // si cancela
                                     if (opcion == JOptionPane.CANCEL_OPTION) {
@@ -73,16 +79,36 @@ public class Main {
                                         }
                                     }
 
-                                    // validar el peso
                                     if (!cancelado) {
+                                        // validar el peso de la Gallina
                                         try {
                                             peso = Double.parseDouble(pesoTxt.getText());
-
+                                            // Cumple con los requisitos
+                                            if (peso < 1.7 || peso > 2.2) {
+                                                JOptionPane.showMessageDialog(null, "El peso debe estar entre 1,7 - 2,2 kg", "Peso no admitido - Registro Gallina", JOptionPane.ERROR_MESSAGE);
+                                                bien = false;
+                                            }
+                                            
                                             // # trabajo con el objeto
                                         } catch (NumberFormatException e) {
-                                            JOptionPane.showMessageDialog(null, "El peso debe ser un numero", "Error", JOptionPane.ERROR_MESSAGE);
+                                            JOptionPane.showMessageDialog(null, "El peso debe ser un numero", "Peso no admitido - Registro Vaca", JOptionPane.ERROR_MESSAGE);
                                             bien = false;
                                         }
+                                    }
+
+                                    // validamos la edad de la Gallina
+                                    try {
+                                        edad = Integer.parseInt(edadTxt.getText());
+                                        // Cumple con los requisitos
+                                        if (edad < 1) {
+                                            JOptionPane.showMessageDialog(null, "La edad debe ser 1 0 mayor para producción", "Edad no admitida - Registro Gallina", JOptionPane.ERROR_MESSAGE);
+                                            bien = false;
+                                        }
+                                        
+                                        // # trabajo con el objeto
+                                    } catch (NumberFormatException e) {
+                                        JOptionPane.showMessageDialog(null, "La edad debe ser un numero entero", "Error", JOptionPane.ERROR_MESSAGE);
+                                        bien = false;
                                     }
                                 } while (!bien);
                                 break;
@@ -91,7 +117,8 @@ public class Main {
                                 panelV = new JPanel(new BorderLayout(5,5));
                                 labels = new JPanel(new GridLayout(0,1,2,2));
                                 labels.add(new JLabel("Raza", SwingConstants.TRAILING));
-                                labels.add(new JLabel("Peso", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Edad", SwingConstants.TRAILING));
+                                labels.add(new JLabel("Peso (kg)", SwingConstants.TRAILING));
                                 panelV.add(labels, BorderLayout.LINE_START);
                                 
                         
@@ -99,15 +126,20 @@ public class Main {
                                 raza = new JTextField("Brahman");
                                 controls.add(raza);
                                 raza.setEditable(false);
+                                edadTxt = new JTextField();
+                                controls.add(edadTxt);
                                 pesoTxt = new JTextField();
                                 controls.add(pesoTxt);
                                 panelV.add(controls, BorderLayout.CENTER);
-                        
+                                
                                 do {
+                                    edadTxt.setText("");
+                                    pesoTxt.setText("");
                                     bien = true;
                                     cancelado = false;
                                     opcion = JOptionPane.showConfirmDialog(null, panelV, "Registrando Vaca", JOptionPane.OK_CANCEL_OPTION);
                                     System.out.println("Raza: " + raza.getText());
+                                    System.out.println("Edad: " + edadTxt.getText());
                                     System.out.println("Peso: " + new String(pesoTxt.getText()));
                                     
                                     // si cancela
@@ -123,14 +155,35 @@ public class Main {
                                         }
                                     }
 
-                                    // validar el peso
                                     if (!cancelado) {
+                                        // validar el peso de la vaca
                                         try {
                                             peso = Double.parseDouble(pesoTxt.getText());
+                                            // Cumple con los requisitos
+                                            if (peso < 450 || peso > 550) {
+                                                JOptionPane.showMessageDialog(null, "El peso debe estar entre 450 - 550 kg", "Error", JOptionPane.ERROR_MESSAGE);
+                                                bien = false;
+                                            }
+                                            
                                             // # trabajo con el objeto
 
                                         } catch (NumberFormatException e) {
                                             JOptionPane.showMessageDialog(null, "El peso debe ser un numero", "Error", JOptionPane.ERROR_MESSAGE);
+                                            bien = false;
+                                        }
+
+                                        try {
+                                            edad = Integer.parseInt(edadTxt.getText());
+                                            // Cumple con los requisitos
+                                            if (edad < 2 || edad > 12) {
+                                                JOptionPane.showMessageDialog(null, "La edad de la Vaca debe estar en 2 - 12 años para Producción", "Edad fuera de rango - Registro Vaca", JOptionPane.ERROR_MESSAGE);
+                                                bien = false;
+                                            }
+                                            
+                                            // # trabajo con el objeto
+
+                                        } catch (NumberFormatException e) {
+                                            JOptionPane.showMessageDialog(null, "La edad debe ser un numero", "Error ingresando un numero", JOptionPane.ERROR_MESSAGE);
                                             bien = false;
                                         }
                                     }
